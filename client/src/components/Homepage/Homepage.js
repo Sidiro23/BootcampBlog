401b3713c730497fbf1b19d7af866196b7d3c406
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-02 19:31:53 -0500

    Pulling

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Blog from './components/Blog/Blog';
+import Comment from './components/Comment/Comment'
 
 export default function Homepage() {
   return (
     <div className="container-home">
       <h1>Home</h1>
+      <Blog />
+      <Comment />
     </div>
   );
 }
