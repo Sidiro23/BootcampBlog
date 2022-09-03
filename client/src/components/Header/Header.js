1dd12c17a7ff84f176d37ea5933f15f93e81976a
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-09-03 13:34:52 -0500

    added style on header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="bg-primary bg-dark text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1 className="m-0">Bootcamp Blog</h1>
         <p className="m-0">Text goes here</p>
