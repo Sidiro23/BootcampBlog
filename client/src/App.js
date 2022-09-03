cccaf43f1a9b157036e26c48845e14da76fa4ab0
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-02 19:20:18 -0500

    Added Comment component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Homepage from './components/Homepage/Homepage'
-// import Header from './components/Header/Header'
+import Comment from './components/Comment/Comment'
 import Footer from './components/Footer/Footer'
-import Login from './components/LogIn/LogIn'
-import Signup from './components/SignUp/SignUp'
+import Blog from './components/Blog/Blog';
+// import Login from './components/LogIn/LogIn'
+// import Signup from './components/SignUp/SignUp'
+// import Header from './components/Header/Header'
 
 
 
@@ -14,9 +16,11 @@ function App(){
   return (
     <div >
       <Homepage />
+      <Blog />
+      <Comment />
       {/* <Header/> */}
-      <Login />
-      <Signup />
+      {/* <Login />
+      <Signup /> */}
       <Footer/>
     </div>
   )
--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./comment.css";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Comment() {
   return (
-    <div>Comment</div>
-  )
+    <div>
+      <Button onClick={useState}>Comment</Button>
+      <Modal show={false} id="modal">
+        <Modal.Header>
+          <h5>Comment</h5>
+          <Button className="btn-close"></Button>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="mb-3 userDiv">
+              <div className="avatar"></div>
+              <label className="col-form-label username">Matt_05</label>
+            </div>
+            <div className="mb-3">
+              <p>This is where the post content will go</p>
+            </div>
+            <div className="mb-3">
+              <label className="col-form-label comment">Comment:</label>
+              <textarea class="form-control"></textarea>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="submitBtn">Submit</button>
+          <button className="closeBtn">Close</button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
