e15b3f58c576e5e8ebd02747da8874b0e0833cea
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-03 13:28:11 -0500

    Tied in the routes to the nav bar, Navbar sends user to page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Homepage from './components/Homepage/Homepage'
-import Header from './components/Header/Header'
+import Homepage from "./components/Homepage/Homepage";
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import Comment from './components/Comment/Comment'
-import Blog from './components/Blog/Blog';
+import SignUp from "./components/SignUp/SignUp";
+// import Comment from './components/Comment/Comment';
+// import Blog from './components/Blog/Blog';
 import Login from './components/LogIn/LogIn'
-import NavBar from './components/NavBar/NavBar'
-// import Signup from './components/SignUp/SignUp'
-// import Login from './components/LogIn/LogIn'
-//import Signup from './components/SignUp/SignUp'
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -22,19 +21,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div>
-          <NavBar/>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div>
+            <NavBar />
+          </div>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              {/* <Blog />
+              <Comment /> */}
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <div className="container">
-          <Login />
-          <Homepage />
-          <Blog />
-          <Comment />
-        </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,121 +1,152 @@
-import React, {useState} from 'react'
-import { checkPassword, validateEmail, validateName } from '../utils/helpers'
+import React, { useState } from "react";
+import "./Signup.css";
+
+export default function SignUp() {
+  return (
+    <div>
+      <div className="container-signup">
+        <h2>Sign Up</h2>
+        <form>
+          <div className="container-user">
+            <input type="text" name="" required=""></input>
+            <label>Email</label>
+          </div>
+          <div className="container-user">
+            <input type="text" name="" required=""></input>
+            <label>Username</label>
+          </div>
+          <div className="container-user">
+            <input type="password" name="" required=""></input>
+            <label>Pasword</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign Up
+          </a>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+// import { checkPassword, validateEmail, validateName } from '../utils/helpers'
 // const bcrypt = require ('bcrypt');
 // const saltRounds = 10;
+// import {resolvers} from '../../../../server/schemas/resolvers'
 
-import {resolvers} from '../../../../server/schemas/resolvers'
+// function Signup(){
+//     const [email, setEmail]=useState('');
+//     const [userName, setUsername]=useState('');
+//     const [firstName, setFirstName]=useState('');
+//     const [lastName, setLastName]=useState('');
+//     const [password, setPassword]=useState('');
+//     const [errorMessage, setErrorMessage]=useState('');
 
-function Signup(){
-    const [email, setEmail]=useState('');
-    const [userName, setUsername]=useState('');
-    const [firstName, setFirstName]=useState('');
-    const [lastName, setLastName]=useState('');
-    const [password, setPassword]=useState('');
-    const [errorMessage, setErrorMessage]=useState('');
+//     const handleInputChange = (e) =>{
+//         const {target}=e;
+//         const inputType = target.name;
+//         const inputValue = target.value;
 
-    const handleInputChange = (e) =>{
-        const {target}=e;
-        const inputType = target.name;
-        const inputValue = target.value;
+//         if (inputType === 'email'){
+//             setEmail(inputValue);
+//         } else if (inputType === 'userName'){
+//             setUsername(inputValue);
+//         }else if (inputType === 'firstName'){
+//             setFirstName(inputValue);
+//         }else if (inputType ==='lastName'){
+//             setLastName(inputValue)
+//         }else if (inputType ==='password'){
+//             setPassword(inputValue);
+//         }
+//     };
 
-        if (inputType === 'email'){
-            setEmail(inputValue);
-        } else if (inputType === 'userName'){
-            setUsername(inputValue);
-        }else if (inputType === 'firstName'){
-            setFirstName(inputValue);
-        }else if (inputType ==='lastName'){
-            setLastName(inputValue)
-        }else if (inputType ==='password'){
-            setPassword(inputValue);
-        }
-    };
+//     const handleFormSubmit = (e) =>{
+//         e.preventDefault();
+//         if (!validateEmail(email)|| !userName){
+//             setErrorMessage('Email or Username is invalid');
+//             return
+//         }
+//         if(!checkPassword(password)){
+//             setErrorMessage(
+//                 `Choose a more secure password for ${userName}`
+//             );
+//             return
+//         }
+//         if (!validateName(firstName)){
+//             setErrorMessage(
+//                 'Please enter a first Name'
+//             );
+//             return
+//         }
+//         if(!validateName(lastName)){
+//             setErrorMessage(
+//                 'Please enter a last name'
+//             );
+//             return
+//         }
+//         alert(`Welcome ${userName}`);
+//         setEmail('');
+//         setUsername('');
+//         setFirstName('');
+//         setLastName('');
+//         setPassword('');
 
-    const handleFormSubmit = (e) =>{
-        e.preventDefault();
-        if (!validateEmail(email)|| !userName){
-            setErrorMessage('Email or Username is invalid');
-            return
-        }
-        if(!checkPassword(password)){
-            setErrorMessage(
-                `Choose a more secure password for ${userName}`
-            );
-            return 
-        }
-        if (!validateName(firstName)){
-            setErrorMessage(
-                'Please enter a first Name'
-            );
-            return
-        }
-        if(!validateName(lastName)){
-            setErrorMessage(
-                'Please enter a last name'
-            );
-            return
-        }
-        alert(`Welcome ${userName}`);
-        setEmail('');
-        setUsername('');
-        setFirstName('');
-        setLastName('');
-        setPassword('');
+//     };
+//     return(
+//     <div className="row">
+//     <div className="col-md-12">
+//       <form action="index.html" method="post">
+//         <h1> Sign Up </h1>
 
-    
-    };
-    return(
-    <div className="row">
-    <div className="col-md-12">
-      <form action="index.html" method="post">
-        <h1> Sign Up </h1>
-        
-           <label for = 'firstName'>First Name:</label> 
-           <input 
-           value ={firstName}
-           type = 'text' 
-           name ='firstName'
-           onChange = {handleInputChange}
-           /> 
-           <label for = 'lastName'>Last Name:</label> 
-           <input 
-           value ={lastName}
-           type = 'text' 
-           name ='lastName'
-           onChange = {handleInputChange}
-           />     
-           <label for="userName">Username:</label>
-           <input 
-           value={userName}
-           type="text" 
-           name="useName"
-           onChange = {handleInputChange}
-           />
-           <label for="email">Email:</label>
-           <input
-           value ={email} 
-           type="email" 
-           name="email"
-           onChange={handleInputChange}
-           />
-           <label for="password">Password:</label>
-           <input
-           value = {password} 
-           type="password" 
-           name="password"
-           onChange ={handleInputChange}
-           />
-      
-        <button type="button" onClick={[handleFormSubmit, resolvers]}>Sign Up</button>
-        
-    </form>
-    {errorMessage &&(
-        <div>
-            <p className='error-text'>{errorMessage}</p>
-        </div>
-    )}
-    </div>
-    </div>
-  )
-}
-export default Signup;
+//            <label for = 'firstName'>First Name:</label>
+//            <input
+//            value ={firstName}
+//            type = 'text'
+//            name ='firstName'
+//            onChange = {handleInputChange}
+//            />
+//            <label for = 'lastName'>Last Name:</label>
+//            <input
+//            value ={lastName}
+//            type = 'text'
+//            name ='lastName'
+//            onChange = {handleInputChange}
+//            />
+//            <label for="userName">Username:</label>
+//            <input
+//            value={userName}
+//            type="text"
+//            name="useName"
+//            onChange = {handleInputChange}
+//            />
+//            <label for="email">Email:</label>
+//            <input
+//            value ={email}
+//            type="email"
+//            name="email"
+//            onChange={handleInputChange}
+//            />
+//            <label for="password">Password:</label>
+//            <input
+//            value = {password}
+//            type="password"
+//            name="password"
+//            onChange ={handleInputChange}
+//            />
+
+//         <button type="button" onClick={[handleFormSubmit, resolvers]}>Sign Up</button>
+
+//     </form>
+//     {errorMessage &&(
+//         <div>
+//             <p className='error-text'>{errorMessage}</p>
+//         </div>
+//     )}
+//     </div>
+//     </div>
+//   )
+// }
+// export default Signup;
