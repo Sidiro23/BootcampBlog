985e2bd1fca0e981c84e0593f63c3dd780371ea0
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-09-04 19:15:04 -0500

    Styled blog post component

--- client/src/components/BlogList/index.js
+++ client/src/components/BlogList/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import "./blogList.css";
 import CommentCard from "../Comment/CommentCard";
 import Comment from "../Comment/Comment";
 
@@ -13,23 +14,26 @@ const BlogList = ({ blogs, title }) => {
       <h3>{title}</h3>
       {blogs &&
         blogs.map((blog) => (
-          <div key={blog._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {blog.blogAuthor} <br />
-              <span style={{ fontSize: "1rem" }}>
-                had this blogs on {blog.createdAt}
-              </span>
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{blog.blogText}</p>
-            </div>
-            <Button>Read More</Button>
-            {/* <Link
+          <div key={blog._id} className="container-blog">
+            <div className="card">
+              <div className="card-header">
+                <div className="card-avatar"></div>
+                <p className="card-author">{blog.blogAuthor}</p>
+                <p className="createdAt">
+                  {blog.createdAt}
+                </p>
+              </div>
+              <div className="card-body">
+                <p className="card-text">{blog.blogText}</p>
+                <Button>Read More</Button>
+              </div>
+              {/* <Link
               className="btn btn-primary btn-block btn-squared"
               to={`/blogs/${blog._id}`}
             >
               Join the discussion on this blogs.
             </Link> */}
+            </div>
           </div>
         ))}
     </div>
