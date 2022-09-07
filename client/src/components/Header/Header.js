dc8803db59cbd20d245fa6222bca979b6f6076e8
Aaron Anglin <101485583+aanglin@users.noreply.github.com>
2022-09-07 14:39:38 -0500

    finished readme and changed title

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-dark text-light py-3 flex-row">
       <div className="headerDiv">
         <h1 className="m-0">Bootcamp Blog</h1>
-        <p className="m-0">Text goes here</p>
+        {/* <p className="m-0">Text goes here</p> */}
       </div>
     </header>
   );
