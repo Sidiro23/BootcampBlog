aacca8738b7184052c42c2e28609a6d6897b0a1e
Sergey Holin <holin_sergey@yahoo.com>
2022-09-06 19:18:03 -0700

    created functionality for create new blog on dashboard

--- client/src/components/NewBlog/index.js
+++ client/src/components/NewBlog/index.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import { Button, Modal } from "react-bootstrap";
-import { useState } from "react";
-import Auth from "../../utils/auth";
+import { ADD_BLOG } from '../../utils/mutations';
+import { QUERY_BLOGS, QUERY_ME } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
 
 const NewBlog = () => {
+  const [blogText, setBlogText] = useState('');
+
+  const [addBlog, { error }] = useMutation(ADD_BLOG, {
+    update(cache, { data: { addBlog } }) {
+      try {
+        const { blogs } = cache.readQuery({ query: QUERY_BLOGS });
+
+        cache.writeQuery({
+          query: QUERY_BLOGS,
+          data: { blogs: [addBlog, ...blogs] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, blogs: [...me.blogs, addBlog] } },
+      });
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addBlog({
+        variables: {
+          blogText,
+          blogAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setBlogText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'blogText' && value.length) {
+      setBlogText(value);
+    }
+  };
+
+
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
@@ -18,13 +71,17 @@ const NewBlog = () => {
           <Button className="btn-close" onClick={handleClose}></Button>
         </Modal.Header>
         <Modal.Body>
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <div className="mb-3">
               <label className="col-form-label comment">Text:</label>
-              <textarea name="commentText" className="form-control"></textarea>
+              <textarea name="blogText" 
+              className="form-control"
+              value={blogText}
+              onChange={handleChange}>
+              </textarea>
             </div>
             <Modal.Footer>
-              <Button onClick={handleClose}>Save</Button>
+              <Button onClick={handleClose} type="submit">Save</Button>
             </Modal.Footer>
           </form>
         </Modal.Body>
