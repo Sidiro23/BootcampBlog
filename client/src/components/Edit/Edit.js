7b2edbe012f4cfea5e87090b2baff8720da59e75
Sergey Holin <holin_sergey@yahoo.com>
2022-09-06 18:55:32 -0700

    working on update & delete blog

--- client/src/components/Edit/Edit.js
+++ client/src/components/Edit/Edit.js
@@ -3,8 +3,98 @@ import "./edit.css";
 import { Button, Modal } from "react-bootstrap";
 import { useState } from "react";
 import Auth from "../../utils/auth";
+import { useMutation } from '@apollo/client';
+import { UPDATE_BLOG } from '../../utils/mutations';
+import { QUERY_BLOGS, QUERY_ME } from '../../utils/queries';
 
 const Edit = () => {
+  // VERSION 1--------------------------------------------------------------------------------
+  // const [blogText, setBlogText] = useState('');
+
+  // const [updateBlog, { error }] = useMutation(UPDATE_BLOG);
+
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     const { data } = await updateBlog({
+  //       variables: {
+  //         blogId,
+  //         blogText,
+  //         blogAuthor: Auth.getProfile().data.username,
+  //       },
+  //     });
+
+  //     setBlogText('');
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+
+  //   if (name === 'blogText' && value.length) {
+  //     setBlogText(value);
+  //     // setCharacterCount(value.length);
+  //   }
+  // };
+//  VERSION 2--------------------------------------------------------------------------------
+const [blogText, setBlogText] = useState('');
+
+// const [characterCount, setCharacterCount] = useState(0);
+
+const [updateBlog, { error }] = useMutation(UPDATE_BLOG, {
+  update(cache, { data: { updateBlog } }) {
+    try {
+      const { blogs } = cache.readQuery({ query: QUERY_BLOGS });
+
+      cache.writeQuery({
+        query: QUERY_BLOGS,
+        data: { blogs: [updateBlog, ...blogs] },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    // update me object's cache
+    const { me } = cache.readQuery({ query: QUERY_ME });
+    cache.writeQuery({
+      query: QUERY_ME,
+      data: { me: { ...me, blogs: [...me.blogs, updateBlog] } },
+    });
+  },
+});
+
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+
+  try {
+    const { data } = await updateBlog({
+      variables: {
+        blogText,
+        blogAuthor: Auth.getProfile().data.username,
+      },
+    });
+
+    setBlogText('');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const handleChange = (event) => {
+  const { name, value } = event.target;
+
+  if (name === 'blogText' && value.length) {
+    setBlogText(value);
+    // setCharacterCount(value.length);
+  }
+};
+// --------------------------------------------------------------------------------------
+// Need delete as well
+// ---------------------------------------------------------------------------------------
+
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
@@ -21,17 +111,19 @@ const Edit = () => {
               <Button className="btn-close" onClick={handleClose}></Button>
             </Modal.Header>
             <Modal.Body>
-              <form>
+              <form  onSubmit={handleFormSubmit}>
                 <div className="mb-3">
                   <label className="col-form-label comment">Edit Text:</label>
                   <textarea
-                    name="commentText"
+                    name="blogText"
                     className="form-control"
+                    value={blogText}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
                 <Modal.Footer>
                   <Button onClick={handleClose}>Delete</Button>
-                  <Button onClick={handleClose}>Update</Button>
+                  <Button onClick={handleClose} type="submit">Update</Button>
                 </Modal.Footer>
               </form>
             </Modal.Body>
